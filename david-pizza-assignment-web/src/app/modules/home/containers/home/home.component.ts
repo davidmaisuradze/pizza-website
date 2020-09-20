d183b7f01f6437a3c42e0f3a322dbdbf52ee9c04
David Maisuradze <dmaisuradze05231995@gmail.com>
2020-09-20 11:29:00 +0400

    small bugfixes

--- david-pizza-assignment-web/src/app/modules/home/containers/home/home.component.ts
+++ david-pizza-assignment-web/src/app/modules/home/containers/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { State } from '../../../auth/reducers/auth.reducer';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AuthSelectors } from '../../../auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent implements OnInit, OnDestroy {
+  private _destroyed$ = new Subject();
+  public user$: Observable<any>;
 
-  ngOnInit() {}
+  constructor(private store: Store<State>) {
+    this.user$ = this.store.pipe(takeUntil(this._destroyed$), select(AuthSelectors.selectUser));
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
 }
--- david-pizza-assignment-web/src/app/shared/components/price-view/price-view.component.ts
+++ david-pizza-assignment-web/src/app/shared/components/price-view/price-view.component.ts
@@ -1,43 +1,46 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CurrencyEnum } from '../../../core/enums/currency.enum';
 import { dollarsToEuro } from '../../helpers/currency.helpers';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
-    selector: 'app-price-view',
-    templateUrl: './price-view.component.html',
-    styleUrls: ['./price-view.component.scss']
+  selector: 'app-price-view',
+  templateUrl: './price-view.component.html',
+  styleUrls: ['./price-view.component.scss']
 })
 export class PriceViewComponent implements OnInit, OnChanges {
-    @Input()
-    public totalPrice = 0;
+  @Input()
+  public totalPrice = 0;
 
-    public priceTitle$ = new BehaviorSubject('');
-    public currencyTypes = CurrencyEnum;
-    public currency = 'DOLLARS';
+  @Input()
+  public idName = '';
 
-    constructor() {
+  public priceTitle$ = new BehaviorSubject('');
+  public currencyTypes = CurrencyEnum;
+  public currency = 'DOLLARS';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.updatePriceTitle(this.totalPrice);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes && changes.totalPrice && changes.totalPrice.currentValue !== changes.totalPrice.previousValue) {
+      this.updatePriceTitle(changes.totalPrice.currentValue);
     }
+  }
 
-    ngOnInit(): void {
-        this.updatePriceTitle(this.totalPrice);
-    }
+  updatePriceTitle(totalPrice: number) {
+    this.priceTitle$.next(
+      this.currency === CurrencyEnum.Dollars
+        ? `$${totalPrice.toFixed(2)}` : `€${dollarsToEuro(totalPrice)}`
+    );
+  }
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes && changes.totalPrice && changes.totalPrice.currentValue !== changes.totalPrice.previousValue) {
-            this.updatePriceTitle(changes.totalPrice.currentValue);
-        }
-    }
-
-    updatePriceTitle(totalPrice: number) {
-        this.priceTitle$.next(
-            this.currency === CurrencyEnum.Dollars
-                ? `$${totalPrice.toFixed(2)}` : `€${dollarsToEuro(totalPrice)}`
-        );
-    }
-
-    onCurrencyChange(e) {
-        this.currency = e.target.value;
-        this.updatePriceTitle(this.totalPrice);
-    }
+  onCurrencyChange(e) {
+    this.currency = e.target.value;
+    this.updatePriceTitle(this.totalPrice);
+  }
 }
